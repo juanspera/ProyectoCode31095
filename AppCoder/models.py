69dfbb816423818c7cc425a6c4781055ab1ef2e1
Luis Herrera <leherreras@gmail.com>
2022-08-24 21:01:52 -0300

    create the unique data in models

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)
-    camada = models.IntegerField()
+    camada = models.IntegerField(unique=True)
 
 
 class Estudiantes(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     profesion = models.CharField(max_length=30)
 
 
