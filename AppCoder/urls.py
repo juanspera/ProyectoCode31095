04fe83b3646ae9c081ac19f8fbb55fb32792cf7c
Luis Herrera <leherreras@gmail.com>
2022-08-29 21:08:48 -0300

    create forms

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from AppCoder.views import curso, entregable, inicio
+from AppCoder.views import *
 
 urlpatterns = [
     path('', inicio, name='AppCoderInicio'),
     path('curso/', curso, name='AppCoderCurso'),
-    path('entregable/', entregable, name='AppCoderEntregable')
+    path('entregable/', entregable, name='AppCoderEntregable'),
+    path('curso_formulario/', curso_formulario, name='AppCoderCursoFormulario'),
+    path('busqueda_camada/', busqueda_camada, name='AppCoderBusquedaCamada'),
+    path('busqueda_camada_post/', busqueda_camada_post, name='AppCoderBusquedaCamadaPost'),
 ]
